c35b0f2b344d3c599cd89fb42ed5e70db1b89fe1
Adam Willis <144346409+willis1927@users.noreply.github.com>
2025-11-03 17:34:48 +0000

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv";
- 
+import path from "path" 
+
+
 import toDoRoutes from "./Routes/toDoRoutes.js"
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -10,15 +12,26 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
+
 
 //middleware
-app.use(cors())
+if (process.env.NODE_ENV !== "production"){app.use(cors())}
+
 app.use(express.json())
 app.use(rateLimiter)
 
 
 app.use("/api/notes", toDoRoutes);
 
+if (process.env.NODE_ENV === "production"){
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+})
+}
 connectDB().then(() =>{
 app.listen(PORT, () => {
     console.log("Serverd started on PORT:5001")
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
+const BASE_URL = import.meta.env.MODE === "development" ? "https://crowded-poltergeist-7v9xg5vq9xpw2pvx4-5001.app.github.dev/api" : "/api"
 const api = axios.create({
-    baseURL: "https://crowded-poltergeist-7v9xg5vq9xpw2pvx4-5001.app.github.dev/api"
+    baseURL: BASE_URL
 })
 
 
